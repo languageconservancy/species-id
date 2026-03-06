5f896ad1eaf24e81260126f6005e57f8cae81853
petevieiratlc <pete.vieira@languageconservancy.org>
2026-03-06 09:54:18 -0700

    fix: typo in script

--- scripts/copy-species-data-to-app.sh
+++ scripts/copy-species-data-to-app.sh
@@ -2,7 +2,7 @@
 set -x
 
 # Copy the species assets to the project
-cp -r external-assets/index.html ./src/assets/species-data/
+cp -r external-assets/index.json ./src/assets/species-data/
 cp -r external-assets/databases ./src/assets/species-data/
 cp -r external-assets/birds ./src/assets/species-data/
 cp -r external-assets/plants ./src/assets/species-data/
