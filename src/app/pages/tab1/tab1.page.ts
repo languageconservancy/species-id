813c5ffd7a24fde8c78a33791a3fbb443ab79cef
petevieiratlc <pete.vieira@languageconservancy.org>
2026-03-09 09:46:23 -0700

    fix: component names

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -5,12 +5,14 @@ import { search, options } from 'ionicons/icons';
 import { ActivatedRoute } from '@angular/router';
 import { SpeciesType } from 'app/models/species.model';
 import { ConfigService } from 'app/services/config.service';
+import { BirdsExplorePage } from '../explore-container/birds-explore.page';
+import { PlantsExplorePage } from '../explore-container/plants-explore.page';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle],
+  imports: [IonHeader, IonToolbar, IonTitle, BirdsExplorePage, PlantsExplorePage],
 })
 export class Tab1Page implements OnInit {
   public domain: SpeciesType = SpeciesType.Bird;
@@ -18,9 +20,7 @@ export class Tab1Page implements OnInit {
 
   get domainLabel(): string {
     const labels = this.configService.getDomainLabels();
-    return this.domain === SpeciesType.Bird
-      ? (labels.bird ?? 'Birds')
-      : (labels.plant ?? 'Plants');
+    return this.domain === SpeciesType.Bird ? (labels.bird ?? 'Birds') : (labels.plant ?? 'Plants');
   }
 
   constructor(
